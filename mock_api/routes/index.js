dd748d79bf5e4e64966e168e3e9a697a855a5ca5
rkorsak <rkorsak@gmail.com>
2016-10-18 15:35:11 -0400

    Updated mock API with new prequal response

--- mock_api/routes/index.js
+++ mock_api/routes/index.js
@@ -1,141 +1,134 @@
 var express = require('express')
 var router = express.Router()
+var uuid = require('node-uuid')
 
 var fakeProducts = {
   'good_credit': {
-    "applicationUrl": "https://applicationqa20.kdc.capitalone.com/icoreapp/jsp/landing.jsp?s=0013262006ER202e1e0f-2d99-4fd1-8796-c533657888d0Z&IARC=ASPEN&experience=ITA",
+    "productId": "1",
+    "applicationUrl": "https://www.capitalone.com?context=applicationUrl",
     "code": "VENTURE01",
-    "name": "Venture® Rewards",
+    "productDisplayName": "Venture® Rewards",
     "priority": 1,
-    "tier": "EXCELLENT CREDIT",
+    "tier": "ExcellentCredit",
     "images": [{
-      "url": {
-        "href": "http://localhost:3002/images/www-venture-visa-sig-flat-9-14.png",
-        "method": "GET",
-        "type": "image/png"
-      },
-      "height": 151,
-      "width": 240,
-      "alternateText": "Apply now for the Venture Rewards Card"
-    }, {
-      "url": {
-        "href": "http://localhost:3002/images/JB16760-GenericEMV-Venture-EMV-flat-244x154-06-15.png",
-        "method": "GET",
-        "type": "image/png"
-      },
       "height": 154,
       "width": 244,
-      "alternateText": "Apply now for the Venture Rewards Card"
+      "alternateText": "Apply now for the Venture Rewards Card",
+      "url": "http://localhost:3002/images/JB16760-GenericEMV-Venture-EMV-flat-244x154-06-15.png",
+      "imageType": "CardArt"
     }],
     "terms": {
       "primaryBenefit": "Earn unlimited 2X miles per dollar on every purchase. Plus, earn 40,000 bonus miles once you spend $3,000 on purchases within the first 3 months.",
       "purchaseAprTerms": "13.24% to 23.24% variable APR",
       "balanceTransferTerms": "13.24% to 23.24% variable APR; No Transfer Fee",
       "annualMembershipFeeTerms": "$0 intro for the first year; $59 after that"
-    }
+    },
+    "additionalInformationUrl": "https://www.capitalone.com?context=additionalInformationUrl"
   },
   'average_credit': {
-    "applicationUrl": "https://applicationqa20.kdc.capitalone.com/icoreapp/jsp/landing.jsp?s=0013262001ER202e1e0f-2d99-4fd1-8796-c533657888d0Z&IARC=ASPEN&experience=ITA",
+    "productId": "2",
+    "applicationUrl": "https://www.capitalone.com?context=applicationUrl",
     "code": "VENTUREONE01",
-    "name": "VentureOne® Rewards",
+    "productDisplayName": "VentureOne® Rewards",
     "priority": 2,
-    "tier": "EXCELLENT CREDIT",
+    "tier": "AverageCredit",
     "images": [{
-      "url": {
-        "href": "http://localhost:3002/images/www-venture-visa-sig-flat-9-14.png",
-        "method": "GET",
-        "type": "image/png"
-      },
-      "height": 151,
-      "width": 240,
-      "alternateText": "Apply now for the VentureOne Rewards Card"
-    }, {
-      "url": {
-        "href": "http://localhost:3002/images/JB16760-Generic-VentureOne-EMV-flat-244x154-06-15.png",
-        "method": "GET",
-        "type": "image/png"
-      },
       "height": 154,
       "width": 244,
-      "alternateText": "Apply now for the VentureOne Rewards Card"
+      "alternateText": "Apply now for the VentureOne Rewards Card",
+      "url": "http://localhost:3002/images/JB16760-Generic-VentureOne-EMV-flat-244x154-06-15.png",
+      "imageType": "CardArt"
     }],
     "terms": {
       "primaryBenefit": "Earn unlimited 1.25 miles per dollar on every purchase. Plus, earn 20,000 bonus miles once you spend $1,000 on purchases within the first 3 months.\r\n",
       "purchaseAprTerms": "0% intro APR until February 2017; 12.24% to 22.24% variable APR after that",
       "balanceTransferTerms": "12.24% to 22.24% variable APR; No Transfer Fee",
       "annualMembershipFeeTerms": "$0"
-    }
+    },
+    "additionalInformationUrl": "https://www.capitalone.com?context=additionalInformationUrl"
   },
   'bad_credit': {
-    "applicationUrl": "https://applicationqa20.kdc.capitalone.com/icoreapp/jsp/landing.jsp?s=0013264008ER202e1e0f-2d99-4fd1-8796-c533657888d0Z&IARC=ASPEN&experience=ITA",
-    "code": "PSECURED01",
-    "name": "Secured MasterCard®",
+    "productId": "3",
+    "applicationUrl": "https://www.capitalone.com?context=applicationUrl",
+    "code": "BLUESTEEL",
+    "name": "Blue Steel MasterCard®",
     "priority": 6,
-    "tier": "REBUILDING CREDIT",
+    "tier": "RebuildingCredit",
     "images": [{
-      "url": {
-        "href": "http://localhost:3002/images/JB16760-MC-Blue-Steel-EMV-flat-244x154-06-15.png",
-        "method": "GET",
-        "type": "image/png"
-      },
       "height": 154,
       "width": 244,
-      "alternateText": "Apply now for the Secured MasterCard Card"
+      "alternateText": "Apply now for the VentureOne Rewards Card",
+      "url": "http://localhost:3002/images/JB16760-MC-Blue-Steel-EMV-flat-244x154-06-15.png",
+      "imageType": "CardArt"
     }],
-    "additionalInformationUrl": "http://www.capitalone.com/credit-cards/LP/secured-mastercard-gen-faq",
     "terms": {
       "primaryBenefit": "Build your credit with responsible use. Refundable deposit of $49, $99 or $200 required.",
       "purchaseAprTerms": "24.99% variable APR",
       "balanceTransferTerms": "24.99% variable APR; No Transfer Fee",
       "annualMembershipFeeTerms": "$0"
-    }
+    },
+    "additionalInformationUrl": "https://www.capitalone.com?context=additionalInformationUrl"
   }
 }
 
-/* GET home page. */
-router.get('/credit-cards/targeted-product-offers', function (req, res, next) {
-  console.info(req.query)
-  var creditRating = req.query.selfAssessedCreditRating
+/* POST to create a prequal check */
+router.post('/credit-cards/prequalifications', function (req, res, next) {
+  console.info(req.body)
   var response = {}
   var status = 200
-  console.info('Determining response based on credit rating (%s)', creditRating)
-  switch (creditRating) {
-    case 'Excellent':
+  var lastName = req.body.lastName
+
+  console.info('Determining response based on last name (%s)', lastName)
+  switch (lastName.toUpperCase().replace(' ', '')) {
+    case 'ONEPRODUCT':
       response = {
         isPrequalified: true,
         products: [ fakeProducts['good_credit'] ]
       }
       break
-    case 'Average':
+    case 'MULTIPROD':
+    case 'MULTIPLEPRODUCTS':
       response = {
         isPrequalified: true,
-        products: [ fakeProducts['average_credit'] ]
+        products: [
+          fakeProducts['good_credit'],
+          fakeProducts['average_credit'],
+          fakeProducts['bad_credit']
+        ]
       }
       break
-    case 'Rebuilding':
+    case 'NOTQUALIFIED':
       response = {
         isPrequalified: false,
-        products: [ fakeProducts['bad_credit'] ]
+        products: [
+          fakeProducts['good_credit'],
+          fakeProducts['average_credit'],
+          fakeProducts['bad_credit']
+        ]
       }
       break
-    case null:
-    case undefined:
-    case '':
+    case 'ERROR':
+      status = 400
       response = {
-        isPrequalified: false,
-        products: []
+        code: 202003,
+        description: 'Simulated error'
       }
       break
     default:
-      status = 400
       response = {
-        code: 999,
-        description: 'Unknown credit rating: ' + creditRating
+        isPrequalified: false,
+        products: [
+          fakeProducts['bad_credit']
+        ]
       }
       break
   }
+
+  var id = { 'prequalificationId': uuid.v4() }
+  var finalResponse = _.assign({}, req.body, id, response)
+
   res.status(status)
-  res.json(response)
+  res.json(finalResponse)
 })
 
 module.exports = router
