fecdac2117a3cba8a78aa4cf8c6f6a15485c33c2
Hogan <matthew.hogan@capitalone.com>
2017-10-27 16:40:59 -0700

    handle prefill

---
+++ public/js/creditoffers.js
@@ -0,0 +1,42 @@
+$(function() {
+  $('.apply-btn').click(function(evt) {
+    if ($('.auth-btn').hasClass('logout-btn')) {
+      $('form[name="prefill-acceptance"]').data('link', $(this).attr('href'))
+      $('#prefill-acceptance').modal().on('hidden.bs.modal', () => {
+        $('#prefill-acceptance .text-danger').addClass('hidden')
+      })
+      evt.preventDefault()
+    }
+  })
+
+  //$('#login').modal('show')
+  $('form[name="prefill-acceptance"]').submit(function(evt) {
+    let link = $(this).data('link')
+    let $spinner = $(this).find('.spinner')
+    $spinner.removeClass('hidden')
+    $.ajax({
+      type: "POST",
+      url: this.action,
+      data: $(this).serialize(), // serializes the form's elements.
+      xhrFields: {
+        withCredentials: true
+      },
+      success: function(data) {
+        link += encodeURIComponent(`&applicantDetailsKey=${data.applicantDetailsKey}`)
+      },
+      error: function(err) {
+        $('#prefill-acceptance .text-danger.hidden').removeClass('hidden')
+      },
+      complete: function() {
+        $spinner.addClass('hidden')
+        let win = window.open(link, '_blank')
+        if (win) {
+          win.focus();
+        } else {
+          alert('Please allow popups for this website');
+        }
+      }
+    });
+    evt.preventDefault();
+  });
+});
