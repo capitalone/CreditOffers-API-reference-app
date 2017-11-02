462c23768427cee5a1c09304709b2a11ddbff118
Hogan <matthew.hogan@capitalone.com>
2017-11-02 15:29:18 -0700

    cleanup and pq ajax removal

--- public/js/creditoffers.js
+++ public/js/creditoffers.js
@@ -40,40 +40,31 @@ $(function() {
     evt.preventDefault();
   });
 
-  $('form[name="prequalification"]').submit(function(evt) {
-    $form = $(this)
-    $.ajax({
-      type: "POST",
-      url: this.action,
-      data: $(this).serialize(), // serializes the form's elements.
-      xhrFields: {
-        withCredentials: true
-      },
-      success: function(data) {
-        $form.off('submit').submit()
-      },
-      error: function(err) {
-        if (err.responseJSON) {
-          $.each($form.serializeArray(), function(i, field) {
-            $formInputParent = $(`[name="${field.name}"]`).parents('.form-group')
-            if (err.responseJSON[field.name]) $formInputParent.addClass('has-error')
-            else $formInputParent.removeClass('has-error')
-          })
-          $form.find('.modal-footer > .text-danger.hidden').removeClass('hidden')
-        } else {
-          $form.off('submit').submit()
-        }
-      }
-      // complete: function() {
-      //   $spinner.addClass('hidden')
-      //   let win = window.open(link, '_blank')
-      //   if (win) {
-      //     win.focus();
-      //   } else {
-      //     alert('Please allow popups for this website');
-      //   }
-      // }
-    });
-    evt.preventDefault();
-  });
+  // $('form[name="prequalification"]').submit(function(evt) {
+  //   evt.preventDefault();
+  //   $form = $(this)
+  //   $.ajax({
+  //     type: "POST",
+  //     url: this.action,
+  //     data: $(this).serialize(), // serializes the form's elements.
+  //     xhrFields: {
+  //       withCredentials: true
+  //     },
+  //     success: function(data) {
+  //       $form[0].submit()
+  //     },
+  //     error: function(err) {
+  //       if (err.responseJSON) {
+  //         $.each($form.serializeArray(), function(i, field) {
+  //           $formInputParent = $(`[name="${field.name}"]`).parents('.form-group')
+  //           if (err.responseJSON[field.name]) $formInputParent.addClass('has-error')
+  //           else $formInputParent.removeClass('has-error')
+  //         })
+  //         $form.find('.modal-footer > .text-danger.hidden').removeClass('hidden')
+  //       } else {
+  //         $form[0].submit()
+  //       }
+  //     }
+  //   });
+  // });
 });
